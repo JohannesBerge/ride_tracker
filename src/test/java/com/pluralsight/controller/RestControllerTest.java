b818606070aa561b6c63cb8a196a7abc7037c100
JohannesBerge <johannes.berge@accenture.com>
2021-04-12 08:21:38 +0200

    normal JDBC insert

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,12 +13,24 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout=5000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		Ride ride = new Ride();
+		ride.setName("car go wheee");
+		ride.setDuration(69);
+
+		restTemplate.put("http://localhost:8080/ride_tracker_war/ride", ride);
+	}
+
+
+	@Test(timeout=5000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
 		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
-				"http://localhost:8080/ride_tracker/rides", HttpMethod.GET,
+				"http://localhost:8080/ride_tracker_war/rides", HttpMethod.GET,
 				null, new ParameterizedTypeReference<List<Ride>>() {
 				});
 		List<Ride> rides = ridesResponse.getBody();
